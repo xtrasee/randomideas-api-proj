5c8f491a52e00c406637374a442dff413e9ece1a
xtrasee <thn018@ucsd.edu>
2023-12-09 22:18:34 -0800

    imported webpack, created modal and idea form functionality

--- public/bundle.js
+++ public/bundle.js
@@ -10,97 +10,117 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/components/IdeaForm.js":
+/*!************************************!*\
+  !*** ./src/components/IdeaForm.js ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction _typeof(o) { \"@babel/helpers - typeof\"; return _typeof = \"function\" == typeof Symbol && \"symbol\" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && \"function\" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? \"symbol\" : typeof o; }, _typeof(o); }\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, \"prototype\", { writable: false }); return Constructor; }\nfunction _toPropertyKey(arg) { var key = _toPrimitive(arg, \"string\"); return _typeof(key) === \"symbol\" ? key : String(key); }\nfunction _toPrimitive(input, hint) { if (_typeof(input) !== \"object\" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || \"default\"); if (_typeof(res) !== \"object\") return res; throw new TypeError(\"@@toPrimitive must return a primitive value.\"); } return (hint === \"string\" ? String : Number)(input); }\nvar IdeaForm = /*#__PURE__*/function () {\n  function IdeaForm() {\n    _classCallCheck(this, IdeaForm);\n    this._formModal = document.querySelector('#form-modal');\n  }\n  _createClass(IdeaForm, [{\n    key: \"addEventListeners\",\n    value: function addEventListeners() {\n      this._form.addEventListener('submit', this.handleSubmit.bind(this));\n    }\n  }, {\n    key: \"handleSubmit\",\n    value: function handleSubmit(e) {\n      e.preventDefault();\n      console.log('SUBMIT');\n    }\n  }, {\n    key: \"render\",\n    value: function render() {\n      this._formModal.innerHTML = \"<form id=\\\"idea-form\\\">\\n        <div class=\\\"form-control\\\">\\n          <label for=\\\"idea-text\\\">Enter a Username</label>\\n          <input type=\\\"text\\\" name=\\\"username\\\" id=\\\"username\\\" />\\n        </div>\\n        <div class=\\\"form-control\\\">\\n          <label for=\\\"idea-text\\\">What's Your Idea?</label>\\n          <textarea name=\\\"text\\\" id=\\\"idea-text\\\"></textarea>\\n        </div>\\n        <div class=\\\"form-control\\\">\\n          <label for=\\\"tag\\\">Tag</label>\\n          <input type=\\\"text\\\" name=\\\"tag\\\" id=\\\"tag\\\" />\\n        </div>\\n        <button class=\\\"btn\\\" type=\\\"submit\\\" id=\\\"submit\\\">Submit</button>\\n      </form>\";\n      this._form = document.querySelector('#idea-form');\n      this.addEventListeners();\n    }\n  }]);\n  return IdeaForm;\n}();\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (IdeaForm);\n\n//# sourceURL=webpack:///./src/components/IdeaForm.js?");
+
+/***/ }),
+
+/***/ "./src/components/Modal.js":
+/*!*********************************!*\
+  !*** ./src/components/Modal.js ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction _typeof(o) { \"@babel/helpers - typeof\"; return _typeof = \"function\" == typeof Symbol && \"symbol\" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && \"function\" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? \"symbol\" : typeof o; }, _typeof(o); }\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, \"prototype\", { writable: false }); return Constructor; }\nfunction _toPropertyKey(arg) { var key = _toPrimitive(arg, \"string\"); return _typeof(key) === \"symbol\" ? key : String(key); }\nfunction _toPrimitive(input, hint) { if (_typeof(input) !== \"object\" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || \"default\"); if (_typeof(res) !== \"object\") return res; throw new TypeError(\"@@toPrimitive must return a primitive value.\"); } return (hint === \"string\" ? String : Number)(input); }\nvar Modal = /*#__PURE__*/function () {\n  function Modal() {\n    _classCallCheck(this, Modal);\n    this._modal = document.querySelector('#modal');\n    this._modalBtn = document.querySelector('#modal-btn');\n    this.addEventListeners();\n  }\n  _createClass(Modal, [{\n    key: \"addEventListeners\",\n    value: function addEventListeners() {\n      this._modalBtn.addEventListener('click', this.open.bind(this));\n      window.addEventListener('click', this.outsideClick.bind(this));\n    }\n  }, {\n    key: \"open\",\n    value: function open() {\n      this._modal.style.display = 'block';\n    }\n  }, {\n    key: \"close\",\n    value: function close() {\n      this._modal.style.display = 'none';\n    }\n  }, {\n    key: \"outsideClick\",\n    value: function outsideClick(e) {\n      if (e.target === this._modal) {\n        close();\n      }\n    }\n  }]);\n  return Modal;\n}();\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Modal);\n\n//# sourceURL=webpack:///./src/components/Modal.js?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-  eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _message__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./message */ \"./src/message.js\");\n/* harmony import */ var _css_style_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./css/style.css */ \"./src/css/style.css\");\n\n\nconsole.log(_message__WEBPACK_IMPORTED_MODULE_0__[\"default\"]);\nvar hello = function hello() {\n  return console.log('hello');\n};\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _fortawesome_fontawesome_free_css_all_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @fortawesome/fontawesome-free/css/all.css */ \"../node_modules/@fortawesome/fontawesome-free/css/all.css\");\n/* harmony import */ var _components_Modal__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/Modal */ \"./src/components/Modal.js\");\n/* harmony import */ var _components_IdeaForm__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/IdeaForm */ \"./src/components/IdeaForm.js\");\n/* harmony import */ var _css_style_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./css/style.css */ \"./src/css/style.css\");\n\n\n\n\nvar modal = new _components_Modal__WEBPACK_IMPORTED_MODULE_1__[\"default\"]();\nvar ideaForm = new _components_IdeaForm__WEBPACK_IMPORTED_MODULE_2__[\"default\"]();\nideaForm.render();\n\n//# sourceURL=webpack:///./src/index.js?");
 
-  /***/ }),
-  
-  /***/ "./src/message.js":
-  /*!************************!*\
-    !*** ./src/message.js ***!
-    \************************/
-  /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-  
-  eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nvar message = {\n  id: 1,\n  text: 'Hello World'\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (message);\n\n//# sourceURL=webpack:///./src/message.js?");
-  
-  /***/ }),
-  
-  /***/ "./src/css/style.css":
-  /*!***************************!*\
-    !*** ./src/css/style.css ***!
-    \***************************/
-  /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-  
-  eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./src/css/style.css?");
-  
-  /***/ })
-  
-  /******/ 	});
-  /************************************************************************/
-  /******/ 	// The module cache
-  /******/ 	var __webpack_module_cache__ = {};
-  /******/ 	
-  /******/ 	// The require function
-  /******/ 	function __webpack_require__(moduleId) {
-  /******/ 		// Check if module is in cache
-  /******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-  /******/ 		if (cachedModule !== undefined) {
-  /******/ 			return cachedModule.exports;
-  /******/ 		}
-  /******/ 		// Create a new module (and put it into the cache)
-  /******/ 		var module = __webpack_module_cache__[moduleId] = {
-  /******/ 			// no module.id needed
-  /******/ 			// no module.loaded needed
-  /******/ 			exports: {}
-  /******/ 		};
-  /******/ 	
-  /******/ 		// Execute the module function
-  /******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-  /******/ 	
-  /******/ 		// Return the exports of the module
-  /******/ 		return module.exports;
-  /******/ 	}
-  /******/ 	
-  /************************************************************************/
-  /******/ 	/* webpack/runtime/define property getters */
-  /******/ 	(() => {
-  /******/ 		// define getter functions for harmony exports
-  /******/ 		__webpack_require__.d = (exports, definition) => {
-  /******/ 			for(var key in definition) {
-  /******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-  /******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-  /******/ 				}
-  /******/ 			}
-  /******/ 		};
-  /******/ 	})();
-  /******/ 	
-  /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-  /******/ 	(() => {
-  /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-  /******/ 	})();
-  /******/ 	
-  /******/ 	/* webpack/runtime/make namespace object */
-  /******/ 	(() => {
-  /******/ 		// define __esModule on exports
-  /******/ 		__webpack_require__.r = (exports) => {
-  /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-  /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-  /******/ 			}
-  /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-  /******/ 		};
-  /******/ 	})();
-  /******/ 	
-  /************************************************************************/
-  /******/ 	
-  /******/ 	// startup
-  /******/ 	// Load entry module and return exports
-  /******/ 	// This entry module can't be inlined because the eval devtool is used.
-  /******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
-  /******/ 	
-  /******/ })()
-  ;
+/***/ }),
+
+/***/ "./src/css/style.css":
+/*!***************************!*\
+  !*** ./src/css/style.css ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./src/css/style.css?");
+
+/***/ }),
+
+/***/ "../node_modules/@fortawesome/fontawesome-free/css/all.css":
+/*!*****************************************************************!*\
+  !*** ../node_modules/@fortawesome/fontawesome-free/css/all.css ***!
+  \*****************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///../node_modules/@fortawesome/fontawesome-free/css/all.css?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
